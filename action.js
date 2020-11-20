81897c394f451b3948eccec02994e8b19e157e81
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-20 16:55:46 +0100

    List of 10 emails with user name and email (array of objects)

--- action.js
+++ action.js
@@ -20,11 +20,16 @@ let app = new Vue({
       axios
       .get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then(function(ajax_call) {
-        console.log(ajax_call);
-        let email = ajax_call.data.response;
-        self.emails_list.push(email);
+        let user_email = ajax_call.data.response;
+        let name = user_email.split('@')[0];
+        let email_object = {
+          user: name,
+          email: user_email,
+        };
+        self.emails_list.push(email_object);
       });
     }
+    console.log(self.emails_list);
   },  // Closing mounted
   methods: {
 
