91c1187f3713840de6df16b286ac7aa846068666
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-20 22:34:32 +0100

    HTML for loading page added (v-if/v-else) & check for duplicate emails (includes)

--- action.js
+++ action.js
@@ -15,23 +15,38 @@ let app = new Vue({
     users_list: []
   },  // Closing data
   mounted: function() {
+    // Keyword "this" cannot be used in axios ".then" unless creating an arrow function
     const self = this;
+    // Generating the random emails
     for (let i = 0; i < self.users_quantity; i++) {
+      // AJAX call
       axios
       .get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then(function(ajax_call) {
+        // Accessing the email value in the AJAX object
         let user_email = ajax_call.data.response;
-        let user_name = user_email.split('@')[0];
-        let email_object = {
-          name: user_name,
-          email: user_email,
-        };
-        self.users_list.push(email_object);
+        let is_included = false;
+        // Scrolling the users_list array (of objects) to check if the random email generated is a duplicate
+        for (let j = 0; j < self.users_list.length && !is_included; j++) {
+          if (self.users_list[j].email.includes(user_email)) {
+            is_included = true;
+          } else {
+            is_included = false;
+          };
+        }
+        // If it is not a duplicate, then it can be pushed in the array
+        if (!is_included) {
+          // Creating the user-name
+          let user_name = user_email.split('@')[0];
+          // Creating the object to be pushed in the users_list array
+          let member_details = {
+            name: user_name,
+            email: user_email,
+          };
+          self.users_list.push(member_details);
+        }
       });
     }
     console.log(self.users_list);
   },  // Closing mounted
-  methods: {
-
-  },  // Closing methods
 });
